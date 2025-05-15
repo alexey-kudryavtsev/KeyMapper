eb3554ae0abc46b5250af4fcdbbb2c4b7832e27b
sds100 <seth.developer@proton.me>
2025-05-15 18:27:20 +0200

    #1394 feat: add cards to set up PRO mode with root or shizuku

--- app/src/main/java/io/github/sds100/keymapper/promode/ProModeScreen.kt
+++ app/src/main/java/io/github/sds100/keymapper/promode/ProModeScreen.kt
@@ -1,6 +1,7 @@
 package io.github.sds100.keymapper.promode
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,7 +17,10 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Android
 import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Checklist
+import androidx.compose.material.icons.rounded.Numbers
 import androidx.compose.material.icons.rounded.WarningAmber
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -33,6 +37,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -41,6 +47,8 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.github.sds100.keymapper.R
 import io.github.sds100.keymapper.compose.KeyMapperTheme
+import io.github.sds100.keymapper.compose.LocalCustomColorsPalette
+import io.github.sds100.keymapper.util.ui.compose.OptionsHeaderRow
 
 @Composable
 fun ProModeScreen(
@@ -118,6 +126,47 @@ private fun Content(
         Spacer(modifier = Modifier.height(16.dp))
 
         if (proModeWarningState is ProModeWarningState.Understood) {
+            OptionsHeaderRow(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                icon = Icons.Rounded.Checklist,
+                text = stringResource(R.string.pro_mode_set_up_title),
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            SetupCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                color = LocalCustomColorsPalette.current.magiskTeal,
+                icon = Icons.Rounded.Numbers,
+                title = stringResource(R.string.pro_mode_root_detected_title),
+                content = {
+                    Text(
+                        text = stringResource(R.string.pro_mode_root_detected_text),
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                },
+                buttonText = stringResource(R.string.pro_mode_root_detected_button),
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            SetupCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                color = LocalCustomColorsPalette.current.shizukuBlue,
+                icon = Icons.Rounded.Android,
+                title = stringResource(R.string.pro_mode_shizuku_detected_title),
+                content = {
+                    Text(
+                        text = stringResource(R.string.pro_mode_shizuku_detected_text),
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                },
+                buttonText = stringResource(R.string.pro_mode_shizuku_detected_button),
+            )
         } else {
             Text(
                 modifier = Modifier.padding(horizontal = 32.dp),
@@ -198,10 +247,78 @@ private fun WarningCard(
     }
 }
 
+@Composable
+private fun SetupCard(
+    modifier: Modifier = Modifier,
+    color: Color,
+    icon: ImageVector,
+    title: String,
+    content: @Composable () -> Unit,
+    buttonText: String,
+    onButtonClick: () -> Unit = {},
+) {
+    OutlinedCard(modifier = modifier) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(modifier = Modifier.padding(horizontal = 16.dp)) {
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = color,
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        ) {
+            content()
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        FilledTonalButton(
+            modifier = Modifier
+                .align(Alignment.End)
+                .padding(horizontal = 16.dp),
+            onClick = onButtonClick,
+            colors = ButtonDefaults.filledTonalButtonColors(
+                containerColor = color,
+                contentColor = LocalCustomColorsPalette.current.contentColorFor(color),
+            ),
+        ) {
+            Text(buttonText)
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
 @Preview
 @Composable
 private fun Preview() {
     KeyMapperTheme {
+        ProModeScreen {
+            Content(
+                proModeWarningState = ProModeWarningState.Understood,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewDark() {
+    KeyMapperTheme(darkTheme = true) {
         ProModeScreen {
             Content(
                 proModeWarningState = ProModeWarningState.Understood,
